26fd960086bb415bd29955a44c8b4b4dac0b2da3
Shadot Hossan Riyad <103817482+shadot-Riyad@users.noreply.github.com>
2026-05-19 21:52:23 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+
 import streamlit as st
 import pickle
 import numpy as np
+import matplotlib
+matplotlib.use('Agg') # সার্ভারে ব্যাকএন্ড ক্র্যাশ হওয়া বন্ধ করার জন্য
 import matplotlib.pyplot as plt
 import shap
 
